c6e4b1e0dd014602f61ac2bbbb60807dc8fac2a2
Andrew Crites <explosion-pills@aysites.com>
2014-03-01 16:42:14 -0500

    Knockout pubsub on login

--- js/app.js
+++ js/app.js
@@ -1,7 +1,40 @@
-define(["knockout", "ko/vm"], function (firebase, ko, vm) {
+define(["knockout", "ko/pubsub", "ko/vm"], function (ko, pubsub, vm) {
+    /**
+     * Handle result of authorized google+ sign in callback
+     */
+    window.signinCallback = function (authResult) {
+        if (authResult['status']['signed_in']) {
+            document.getElementById('signinButton').setAttribute('style', 'display: none');
+            setTimeout(function () {
+                document.querySelector("header").style.WebkitAnimationName = "slideout";
+                document.querySelector("#buttons").style.opacity = 1;
+            }, 600);
+
+            gapi.client.request({
+                path: "/plus/v1/people/me",
+                callback: function (user) {
+                    pubsub.notifySubscribers(user.id, "loggedIn");
+                }
+            });
+        }
+        else {
+            document.getElementById('signinButton').setAttribute('style', 'display: inline');
+        }
+    }
+
     return {
         run: function (config) {
+            var s,
+                po = document.createElement('script');
+
             ko.applyBindings(new vm);
+
+            po.type = 'text/javascript';
+            po.async = true;
+            po.src = 'https://apis.google.com/js/client:plusone.js';
+
+            s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(po, s);
         },
     }
 });
--- js/ko/vm.js
+++ js/ko/vm.js
@@ -2,8 +2,12 @@
  * Knockout ViewModel
  */
 
-define(["jquery", "knockout"], function ($, ko) {
+define(["jquery", "knockout", "ko/pubsub"], function ($, ko, pubsub) {
     return function () {
         var self = this;
+
+        pubsub.subscribe(function (id) {
+            console.log(id);
+        }, self, "loggedIn");
     };
 });
